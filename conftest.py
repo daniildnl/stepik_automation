7405156cd49b0356e1174a5555786e76bdb3354a
Daniil Sukhomlin <daniilsukhomlin@gmail.com>
2023-07-25 19:46:25 +0300

    add locators and page object with methods for login page

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
